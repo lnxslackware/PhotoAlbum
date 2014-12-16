d1e64379395c92bad1e6842137fb2c9134212caa
lnikod4s <lyudmil.nikodimov@gmail.com>
2014-12-16 11:59:19 +0200

    Changed views/home/index.blade.php

--- Source/laravel/app/views/home/index.blade.php
+++ Source/laravel/app/views/home/index.blade.php
@@ -1,5 +1,18 @@
 @extends('master')
 
 @section('content')
-	<h4>Hello</h4>
+	<div class="span3 well">
+		<legend>Would you like to:</legend>
+		<div class="btn-group">
+			<button type="button" class="btn btn-primary" onclick="$('#upload_modal').modal({backdrop: 'static'});"><i class="icon-plus-sign icon-white"></i>Upload a photo?
+			</button>
+			<br/><br/>
+			<button type="button" class="btn btn-primary" onclick="$('#upload_modal').modal({backdrop: 'static'});"><i class="icon-plus-sign icon-white"></i>Add new category?
+			</button>
+			<br/><br/>
+			<button type="button" class="btn btn-primary" onclick="$('#upload_modal').modal({backdrop: 'static'});"><i class="icon-plus-sign icon-white"></i>Add new album?
+			</button>
+			<br/><br/>
+		</div>
+	</div>
 @stop
